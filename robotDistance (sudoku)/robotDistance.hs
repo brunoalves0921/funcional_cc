7f712ec26badd47b89087d144a7fdff3b700be0e
brunoalves0921 <jorge.bruno0921@alu.ufc.br>
2022-06-07 23:37:35 -0300

    trabalho part1 e part2

--- robotDistance (sudoku)/robotDistance.hs
+++ robotDistance (sudoku)/robotDistance.hs
@@ -23,3 +23,20 @@ fit (xs, lim) index value = not (exists valueChar sub)
         valueChar = dig2Char value
 -----------------------------------------GETHOLETEST----------------------------------------
 getHoles xs = elemIndices '.' xs
+------------------------------------------SOLVE---------------------------------------------
+solve (xs, lim) holes hindex
+    | sizeHolesPath == hindex = Just xs
+    | length filteredResult == 0 = Nothing
+    | otherwise = if length filteredResult == 0
+        then Nothing
+        else head filteredResult
+    where
+        sizeHolesPath = length holes
+        possibleValues = zip [0..] (foldl (\acc x -> acc ++ [fit (xs, lim) (holes!!hindex) x]) [] [0..lim])
+        result = [fst x | x <- possibleValues, snd x]
+        variable = [solve (set xs (holes!!hindex) r, lim) holes (hindex+1) | r <- result]
+        filteredResult = [x | x <- variable, isJust x]
+
+----------------------------------------MAIN-----------------------------------------------
+mainSolver xs lim = fromJust $ solve (xs, lim) (getHoles xs) 0
+
